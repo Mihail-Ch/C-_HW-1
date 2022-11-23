944a8ce6826a5058b04973392c1fb067f0f96abc
michail <mihail.chertov@gmail.com>
2022-11-23 23:36:39 +0300

    update hw-1 in function

--- hw.1/Program.cs
+++ hw.1/Program.cs
@@ -5,16 +5,26 @@
  a = -9 b = -3 -> max = -3
  */
 
- Console.WriteLine("Первое число: ");
- int oneNumber = Int16.Parse(Console.ReadLine() ?? "");
- Console.WriteLine("Второе число: ");
+Console.WriteLine("Первое число: ");
+int oneNumber = Int16.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Второе число: ");
 int twoNumber = Int16.Parse(Console.ReadLine() ?? "");
-if (oneNumber > twoNumber) {
-    Console.WriteLine($"{oneNumber} more {twoNumber}");
-} else {
-    Console.WriteLine($"{twoNumber} more {oneNumber}");
-}
 
+int MoreNumber (int first, int second) { 
+    int result = 0;
+    if (first > second) {
+        result = first;
+        Console.WriteLine($"{first} more {second}");
+    } else {
+        result = second;
+        Console.WriteLine($"{second} more {first}");
+    }
+    return result;
+};
+int more = MoreNumber(
+    oneNumber, twoNumber
+);
+Console.WriteLine(more);
 /*
  Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
  2, 3, 7 -> 7
@@ -28,16 +38,24 @@ Console.WriteLine("Второе число: ");
 int numberTwo = Int16.Parse(Console.ReadLine() ?? "");
 Console.WriteLine("Третье число: ");
 int numberThree = Int16.Parse(Console.ReadLine() ?? "");
-int maxNumber = numberOne;
 
-if (numberTwo > maxNumber) {
-    maxNumber = numberTwo;
-}
-if (numberThree > maxNumber) {
-    maxNumber = numberThree;
-}
-Console.WriteLine($"MaxNumber = {maxNumber}");
 
+int MaxNum(int one, int two, int three) {
+     int maxNumber = one;
+     if (two > maxNumber) {
+         maxNumber = two;
+     }
+     if (three > maxNumber) {
+         maxNumber = three;
+     }
+     
+    return maxNumber;
+};
+
+int maxN = MaxNum(
+    numberOne, numberTwo, numberThree
+);
+Console.WriteLine($"MaxNumber = {maxN}");
 /*
  Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
  4 -> да
@@ -47,11 +65,22 @@ Console.WriteLine($"MaxNumber = {maxNumber}");
 
 Console.WriteLine("Введите число: ");
 int number = Int16.Parse(Console.ReadLine() ?? "");
-if (number % 2 == 0) {
-    Console.WriteLine($"{number} - четное число");
-} else {
-    Console.WriteLine($"{number} - нечетное число");
-}
+int ChetnNumber(int a) {
+    int chetn = 0;
+    if (number % 2 == 0) {
+        Console.WriteLine($"{number} - четное число");
+        
+    } else {
+        Console.WriteLine($"{number} - нечетное число");
+        chetn = 1;
+    }
+    return chetn;
+};
+
+int taskChetn = ChetnNumber(
+    4
+);
+Console.WriteLine(taskChetn);
 
 /*
 Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
@@ -59,11 +88,18 @@ if (number % 2 == 0) {
  8 -> 2, 4, 6, 8
 */
 
+
 Console.WriteLine("Введите число: ");
 int num = Int16.Parse(Console.ReadLine() ?? "");
+
+void AllNum(int num) {
 for (int i = 1; i <= num; i++)
 {
     if (i % 2 == 0) {
         Console.WriteLine($"{i}");
     }
 }
+};
+AllNum(
+    5
+);
